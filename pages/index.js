17a78b3ed77caa8aa332b89cff73a46e5da27b2f
a <breno.nunesgalvao@protonmail.ch>
2022-08-02 18:54:08 -0300

    server data fetch

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,44 @@
+import useSWR from 'swr'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 import { Button } from '@nextui-org/react';
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
 const Home = props => {
-  return (
-    <Layout>
-      <div>hello {props.hello.name}</div>
-      <Button>Click me</Button>
+ const { data, error } = useSWR('/api/things', fetcher)
+  if (error) return <div>Failed to load</div>
+    if (!data) return <div>Loading...</div>
+    if (data){
+      console.log(data)
+      return (
+        <Layout>
+          <Button>Click me</Button>
 
-      {props.things.map(thing => (
-        <div key={thing.id} className="post">
-          <p>{thing.name}</p>
-          <p>{thing.content}</p>
-        </div>
-      ))}
-    </Layout>
-  )
+          {data.map(thing => (
+            <div key={thing.id} className="post">
+              <p>{thing.name}</p>
+              <p>{thing.content}</p>
+            </div>
+          ))}
+          </Layout>
+      )
+    }
 }
 
-export const getServerSideProps = async () => {
-  const res = await fetch('http://localhost:3000/api/hello')
-  const hello = await res.json()
-  const res2 = await fetch('http://localhost:3000/api/things')
-  const things = await res2.json()
-  return {
-    props: { hello, things }
-  }
-}
+// export const getServerSideProps = async () => {
+//   console.log("aaaaaaaaaaaaAAA")
+//   let a = useSWR('/api/hello', fetcher)
+//   console.log(a)
+//   const { data, e } = useSWR('/api/hello', fetcher)
+//   // const { things, things_e } = useSWR('/api/things', fetcher)
+//   // const res = await fetch('/api/hello')
+//   // const hello = await res.json()
+//   // const res2 = await fetch('/api/things')
+//   // const things = await res2.json()
+//   return {
+//     props: { hello, things }
+//   }
+// }
 
 export default Home
